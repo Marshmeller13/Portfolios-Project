370774a5ec3a5027427eba86b8cdfc2422bf5a45
noah cox <ncox1@iwu.edu>
2020-10-18 18:30:27 -0500

    Create upload resume routes. Filled out a basic html for creating a resume and enabled the routes for adding resumes. Schema was updated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, g, redirect, url_for, render_template, flash, session
 from sqlite3 import dbapi2 as sqlite3
+
 app = Flask(__name__)
 
 
@@ -49,10 +50,10 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-@app.route('/')
+@app.route('/show_resume')
 def show_resume():
     db = get_db()
-    cur = db.execute('SELECT id, name, age, work_exp, education_hs, education_college, graduated FROM resume_entries ORDER BY id DESC')
+    cur = db.execute('SELECT name, age, work_exp, education_hs, education_college, graduated FROM resume_entries')
     resume_entries = cur.fetchall()
 
     return render_template('resume_template_orig.html', resume_entries = resume_entries)
@@ -62,7 +63,7 @@ def show_resume():
 @app.route('/create_resume', methods=['POST'])
 def create_resume():
     db = get_db()
-    db.execute('insert into resume_entries (name, age, work_exp, education_hs, education_college, graduated)',
+    db.execute('insert into resume_entries (name, age, work_exp, education_hs, education_college, graduated) values (?,?,?,?,?,?)',
                [request.form['name'], request.form['age'], request.form['work_exp'], request.form['education_hs'], request.form['education_college'], request.form['graduated']])
     db.commit()
     flash('Resume Successfully Created')
@@ -71,14 +72,70 @@ def create_resume():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+
+        #get submitted username and password
+        username = request.form['username']
+        password = request.form['password']
+
+        #set error to none to be changed if program encounters an error
+        error = None
+
+        #query database for username
+        db = get_db()
+        user = db.execute('select * from user where username = ?', (username,)).fetchone()
+
+        #if statement for if username matches any users, then checks password associated with account
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        #if no errors encountered, redirect to homepage/dashboard (with framework for setting up a session id within cookies of browser)
+        if error is None:
+            #session.clear()
+            #session['user_id'] = user['id']
+            return render_template('resume_template_orig.html')
+
+        #flash error encountered (if any)
+        flash(error)
+
+    return render_template('login.html')
+
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+def convertToBinaryData(file):
+    #Convert digital data to binary format
+
+
+    blobData = request.files['file'].read()
+
+    return blobData
+
+
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+
+@app.route('/insert_resume', methods=['GET', 'POST'])
+def insert_resume():
+    try:
+        db = get_db()
+        sqlite_insert_blob_query = """ INSERT INTO uploaded_resumes
+                                  (resume) VALUES (?)"""
+        resume = request.files['file']
+        resume = convertToBinaryData(resume)
+        # Convert data into tuple format
+        data_tuple = (resume,)
+        db.execute(sqlite_insert_blob_query, data_tuple)
+        db.commit()
+        print("Image and file inserted successfully as a BLOB into a table")
+        db.close()
+        return render_template('resume_template_orig.html')
+
+    except sqlite3.Error as error:
+        print("Failed to insert blob data into sqlite table", error)
+    finally:
+        if (db):
+            db.close()
--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
 drop table if exists resume_entries;
 create table resume_entries (
-    id integer primary key autoincrement,
     name text not null,
     age text not null,
     work_exp text not null,
@@ -8,3 +7,24 @@ create table resume_entries (
     education_college text not null,
     graduated text not null
 );
+
+
+drop table if exists uploaded_resumes;
+create table uploaded_resumes (
+    resume blob not null
+
+
+);
+
+
+drop table if exists user;
+create table user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username varchar(255) DEFAULT NULL,
+    password varchar(255) DEFAULT NULL,
+    UNIQUE (username)
+);
+
+/*default admin user - password:admin (password is already hashed in below code)*/
+INSERT INTO user (username, password)
+VALUES ('admin','pbkdf2:sha256:150000$G2L505H1$fb07749916f47042d5afafdbc54257f1008e07267731e6a2c170c699b4f72baa');
